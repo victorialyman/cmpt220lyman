7e6a44338c6f066d0ae69ec5e3a02f90019b5862
victorialyman <victoria.lyman1@marist.edu>
2017-01-21 16:41:50 -0500

    These are my files for Project 0

---
+++ prj/0/Driver_prj0.java
@@ -0,0 +1,22 @@
+/**
+  * file: Driver_prj0.java
+  * author: Victoria Lyman
+  * course: CMPT 220
+  * assignment: project 0
+  * due date: Januar 24, 2017
+  * version: 1
+  * This file outputs the statement "Hello World!"
+  */
+
+/**
+  * Driver_prj0
+  * This class implements a text display of
+  * "Hello World!"
+  */
+
+
+public class Driver_prj0 {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
